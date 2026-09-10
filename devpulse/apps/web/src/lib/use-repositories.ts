4e20951b320d8d897017238cc27b197cce5a7919
Abhishek Srivastava <abhishekjs005@gmail.com>
2026-09-11 00:21:33 +0530

    Delete devpulse directory

--- devpulse/apps/web/src/lib/use-repositories.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchRepositories } from "./api";
-
-export function useRepositories() {
-  return useQuery({
-    queryKey: ["repositories"],
-    queryFn: fetchRepositories,
-  });
-}
